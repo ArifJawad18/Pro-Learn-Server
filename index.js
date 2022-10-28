07126b99d27e98880e247e98d00fb291d94c9479
Arifjawad18 <arif.jawad18@gmail.com>
2022-10-28 17:35:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(core());
+
+const categories = require('./data/categories');
+const allcoures = require('./data/allcoures');
+
+app.get('/', (req, res) => {
+    res.send('Course API Running');
+});
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/category:/id', (req, res) => {
+    const id = req.params.id;
+    
+})
+
+app.get('/allcoures/:id',(req, res) => {
+    const id = req.params.id;
+    const selelectedallcourse = allcoures.find(n => n.id === id);
+    res.setDefaultEncoding(selelectedallcourse); 
+})
+
+app.listen(port, () => {
+    console.log('Pro Learn Server running on port', port);
+})
